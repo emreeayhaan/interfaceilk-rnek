4decd1b2cd8e71505e2c455c9c365c19c5984c69
Emre <emre-ayhan-00@hotmail.com>
2021-06-23 10:29:55 +0300

    Add project files.

---
+++ interfaceandabsractdemo/Interface1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interfaceilkörnek
+{
+     public  interface IArabasur
+    {
+        void calistir();
+        void vitesarttır();
+        void parket();  
+        void stopet();
+    }
+    public class Araba : IArabasur
+    { 
+        public void calistir()
+        {
+            Console.WriteLine("araba çalıştı");
+        }
+        public void vitesarttır()
+
+        {
+            Console.WriteLine("vites arttırıldı");
+        }
+        public void parket()
+        {
+            Console.WriteLine("park edildi");
+        }
+        public void stopet()
+        {
+            Console.WriteLine("stop");
+        }
+        
+    }
+        
+    
+}
+
---
+++ interfaceandabsractdemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace interfaceilkörnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Araba Ford = new Araba();
+            Ford.calistir();
+            Ford.vitesarttır();
+            Ford.parket();
+            Ford.stopet();
+            Console.ReadKey();
+
+        }       
+    }
+}
